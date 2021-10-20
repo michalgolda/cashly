a3a79cbbc96f62375df8a1c5d797e79f86fcffc6
Maklero <michalgolda@vp.pl>
2021-10-20 16:31:45 +0200

    feat: create theme

---
+++ cashly_web/src/styles/theme.js
@@ -0,0 +1,27 @@
+const theme = {
+    colors: {
+        primary: "#011627",
+        secondary: "#fff",
+        grey: "#CDCDCD"
+    },
+    font: {
+        family: "'Montserrat', sans-seri",
+        sizes: {
+            h1: "2.488rem",
+            h2: "2.074rem",
+            h3: "1.728rem",
+            h4: "1.44rem",
+            h5: "1.2rem",
+            h6: ".833rem"
+        },
+        weights: {
+            light: "300",
+            regular: "400",
+            medium: "500",
+            semiBold: "600",
+            bold: "700"
+        }
+    }
+}
+
+export default theme;
