b0c704874a22a5bd00add1affc83d3c4f1383c6d
Maklero <michalgolda@vp.pl>
2021-10-20 16:31:04 +0200

    feat: add global styles

---
+++ cashly_web/src/styles/global.js
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    body {
+        color: ${({ theme }) => theme.colors.primary};
+        font-family: ${({ theme }) => theme.font.family};
+        font-weight: ${({ theme }) => theme.font.weights.regular};
+    }
+`;
+
+export default GlobalStyle;
