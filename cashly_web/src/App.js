a3f64847bd243f2b54f98cae1e92b8cb04121d53
Maklero <michalgolda@vp.pl>
2021-10-20 16:25:59 +0200

    feat: add ThemeProvider and GlobalStyle

--- cashly_web/src/App.js
+++ cashly_web/src/App.js
@@ -1,24 +1,12 @@
-import styled from 'styled-components';
-
-const Container = styled.div`
-    width: 100vw;
-    height: 100vh;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-`;
-
-const Greeting = styled.h1`
-    font-size: 5rem;
-    font-weight: 500;
-`;
+import { ThemeProvider } from "styled-components";
+import { theme, GlobalStyle } from "./styles";
 
 function App() {
     return (
-        <Container>
-            <Greeting>Hello, World!</Greeting>
-        </Container>
-    ); 
+        <ThemeProvider theme={theme}>
+            <GlobalStyle />
+        </ThemeProvider>
+    );
 }
 
 export default App;
