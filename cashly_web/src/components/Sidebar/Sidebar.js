777f35c2f51c0d9710335c5da9eaa1689aa86888
Michał <michalgolda@vp.pl>
2021-10-26 18:22:42 +0200

    feat: add sidebar component

---
+++ cashly_web/src/components/Sidebar/Sidebar.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+import { Button } from "../../components";
+
+const StyledContainer = styled.nav`
+    display: grid;
+    max-width: 300px;
+    grid-row-gap: 1rem;
+    height: calc(100vh - 67px);
+    grid-auto-rows: min-content;
+    padding: 1rem 2rem 1rem 2rem;
+    border-right: 1px solid ${({ theme }) => theme.colors.gray500};
+`;
+
+export default function Sidebar() {
+    return (
+        <StyledContainer>
+            <Button size="small" variant="primaryOutline" fullWidth>Wydatki</Button>
+            <Button size="small" variant="primaryOutline" fullWidth>Kategorie wydatków</Button>
+        </StyledContainer>
+    );
+}
+
