ca1ea8eda6b0cd03ef92c4609eac1f720728fd70
Michał <michalgolda@vp.pl>
2021-10-26 17:23:00 +0200

    feat: add button component

--- cashly_web/src/components/Button/Button.js
+++ cashly_web/src/components/Button/Button.js
@@ -1,22 +1,86 @@
-import styled from "styled-components";
+import PropTypes from "prop-types";
+import styled, { css } from "styled-components";
+
+const buttonStyleSizes = {
+    small: css`font-size: ${({ theme }) => theme.font.sizes.h6};`,
+    medium: css`font-size: ${({ theme }) => theme.font.sizes.h5};`,
+    large: css`font-size: ${({ theme }) => theme.font.sizes.h4};`
+};
+
+const buttonStyleVariants = {
+    primary: css`
+        color: ${({ theme }) => theme.colors.white};
+        background-color: ${({ theme }) => theme.colors.primary400};
+
+        &:hover { background-color: ${({ theme }) => theme.colors.primary500}; }
+        
+        &:active { background-color: ${({ theme }) => theme.colors.primary300}; }
+    `,
+    primaryOutline: css`
+        color: ${({ theme }) => theme.colors.primary400};
+        background-color: ${({ theme }) => theme.colors.white};
+        border: 2px solid ${({ theme }) => theme.colors.primary400};
+
+        &:hover { 
+            color: ${({ theme }) => theme.colors.white};
+            background-color: ${({ theme }) => theme.colors.primary500}; 
+        }
+
+        &:active {
+            color: ${({ theme }) => theme.colors.white};
+            background-color: ${({ theme }) => theme.colors.primary300};
+        }
+    `
+};
 
 const StyledButton = styled.button`
+    border: none;
+    outline: none;
+    display: block;
+    cursor: pointer;
     transition: .15s;
-    background-color: transparent;
+    border-radius: 2px;
+    display: inline-block;
     padding: .5rem 1.5rem .5rem 1.5rem;
-    color: ${({ theme }) => theme.colors.primary};
     font-family: ${({ theme }) => theme.font.family};
-    font-size: ${({ theme }) => theme.font.sizes.h6};
-    border: 1px solid ${({ theme }) => theme.colors.primary};
     font-weight: ${({ theme }) => theme.font.weights.semiBold};
 
-    &:hover { 
-        cursor: pointer;
-        color: ${({ theme }) => theme.colors.secondary};
-        background-color: ${({ theme }) => theme.colors.primary};
-    }
+    
+    ${({ size }) => buttonStyleSizes[size]};
+    ${({ variant }) => buttonStyleVariants[variant]};
+
+    ${({ fullWidth }) => fullWidth && css`width: 100%;`};
+    ${({ as }) => as === "a" && css`text-align: center; text-decoration: none;`};
 `;
 
-export default function Button({ children }) {
-    return <StyledButton>{children}</StyledButton>;
+function Button(props) {
+    const { children, variant, size, as } = props;
+
+    return (
+        <StyledButton
+            as={as}
+            size={size} 
+            variant={variant}
+            {...props}
+        >
+            {children}
+        </StyledButton>
+    );
 }
+
+Button.defaultProps = {
+    as: "button",
+    size: "medium",
+    fullWidth: false,
+    variant: "primary" 
+};
+
+Button.propTypes = {
+    fullWidth: PropTypes.bool,
+    children: PropTypes.node.isRequired,
+    as: PropTypes.oneOf(["button", "a"]),
+    size: PropTypes.oneOf(["small", "medium", "large"]),
+    variant: PropTypes.oneOf(["primary", "primaryOutline"])
+};
+
+export default Button;
