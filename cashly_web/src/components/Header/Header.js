a7d74507366d484007a4e16a0f48d7c3bb255cb0
Michał <michalgolda@vp.pl>
2021-10-26 18:11:17 +0200

    feat: change bottom borderline color, center and change size of text

--- cashly_web/src/components/Header/Header.js
+++ cashly_web/src/components/Header/Header.js
@@ -3,19 +3,20 @@ import styled from "styled-components";
 const StyledContainer = styled.div`
     width: 100%;
     height: auto;
-    padding: 1rem 5rem 1rem 5rem;
-    border-bottom: 1px solid ${({ theme }) => theme.colors.grey};
+    text-align: center;
+    padding: 1rem 2rem 1rem 2rem;
+    border-bottom: 1px solid ${({ theme }) => theme.colors.gray500};
 `;
 
-const StyledTextLogo = styled.p`
-    font-size: ${({ theme }) => theme.font.sizes.h3};
+const StyledText = styled.p`
+    font-size: ${({ theme }) => theme.font.sizes.h2};
     font-weight: ${({ theme }) => theme.font.weights.bold};
 `;
 
 export default function Header() {
     return (
         <StyledContainer>
-            <StyledTextLogo>Cashly</StyledTextLogo>
+            <StyledText>Cashly</StyledText>
         </StyledContainer>
     );
 }
