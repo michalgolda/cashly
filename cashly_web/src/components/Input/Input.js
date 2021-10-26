a4d87121d39391a47c677863ab1d6a5fcbc006e0
Michał <michalgolda@vp.pl>
2021-10-26 18:59:50 +0200

    feat: add input component

---
+++ cashly_web/src/components/Input/Input.js
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+const StyledContainer = styled.div`display: table-caption;`;
+
+const StyledLabel = styled.label`
+    height: min-content;
+    padding: .5rem 1rem .5rem 1rem;
+    color: ${({ theme }) => theme.colors.white};
+    font-size: ${({ theme }) => theme.font.sizes.h6};
+    font-weight: ${({ theme }) => theme.font.weights.semiBold};
+    background-color: ${({ theme }) => theme.colors.primary400};
+`;
+
+const StyledInput = styled.input`
+    outline: none;
+    border-radius: 2px;
+    padding: .5rem 1rem .5rem 1rem;
+    font-size: ${({ theme }) => theme.font.sizes.h5};
+    font-family: ${({ theme }) => theme.font.family};
+    font-weight: ${({ theme }) => theme.font.weights.semiBold};
+    border: 2px solid ${({ theme }) => theme.colors.primary400};
+`;
+
+export default function Input(props) {
+    const { labelText } = props;
+    
+    return (
+        <StyledContainer>
+            {labelText && <StyledLabel data-testid="label">{labelText}</StyledLabel>}
+            <StyledInput {...props} data-testid="input" />
+        </StyledContainer>
+    );
+}
--- cashly_web/src/components/index.js
+++ cashly_web/src/components/index.js
@@ -1,5 +1,6 @@
+import Input from "./Input/Input";
 import Header from "./Header/Header";
 import Button from "./Button/Button";
 import Sidebar from "./Sidebar/Sidebar";
 
-export { Header, Button, Sidebar };
+export { Header, Button, Sidebar, Input };
