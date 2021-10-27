a87a116c0eea66a8b931294e1e1e920cdb1a0b8c
Maklero <michalgolda@vp.pl>
2021-10-27 16:32:49 +0200

    feat: add Router component import

--- cashly_web/src/components/index.js
+++ cashly_web/src/components/index.js
@@ -2,6 +2,7 @@ import Input from "./Input/Input";
 import Header from "./Header/Header";
 import Button from "./Button/Button";
 import Layout from "./Layout/Layout";
+import Router from "./Router/Router";
 import Sidebar from "./Sidebar/Sidebar";
 
 export { 
@@ -9,5 +10,6 @@ export {
     Header, 
     Button, 
     Layout,
+    Router,
     Sidebar 
 };
