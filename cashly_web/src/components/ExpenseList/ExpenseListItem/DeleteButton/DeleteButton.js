b0244b881da917b3ac53846e3177f4c1b993aca9
Michał <michalgolda@vp.pl>
2021-10-29 23:50:46 +0200

    feat: set DeleteButton color to red

--- cashly_web/src/components/ExpenseList/ExpenseListItem/DeleteButton/DeleteButton.js
+++ cashly_web/src/components/ExpenseList/ExpenseListItem/DeleteButton/DeleteButton.js
@@ -3,7 +3,13 @@ import { Button } from "../../../../components";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
-const StyledButton = styled(Button)`background-color: red;`;
+const StyledButton = styled(Button)`
+    border: none;
+    background-color: ${({ theme }) => theme.colors.red400};
+
+    &:hover { background-color: ${({ theme }) => theme.colors.red500}; }
+    &:active { background-color: ${({ theme }) => theme.colors.red300}; }
+`;
 
 export default function DeleteButton(props) {
     return (
