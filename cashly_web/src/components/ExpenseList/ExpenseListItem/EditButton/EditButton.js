a0f6f184d621cc8519d89939cf216c9ff19738b8
Michał <michalgolda@vp.pl>
2021-10-29 23:50:58 +0200

    feat: set EditButton to blue color

--- cashly_web/src/components/ExpenseList/ExpenseListItem/EditButton/EditButton.js
+++ cashly_web/src/components/ExpenseList/ExpenseListItem/EditButton/EditButton.js
@@ -3,7 +3,13 @@ import { Button } from "../../../../components";
 import { faEdit } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
-const StyledButton = styled(Button)`background-color: blue;`;
+const StyledButton = styled(Button)`
+    border: none;
+    background-color: ${({ theme }) => theme.colors.blue400};
+
+    &:hover { background-color: ${({ theme }) => theme.colors.blue500}; }
+    &:active { background-color: ${({ theme }) => theme.colors.blue300}; }
+`;
 
 export default function EditButton(props) {
     return (
