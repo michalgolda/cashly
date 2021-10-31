5cb76de80296ddba761ce0884c85127d7e3ae8e9
Michał <michalgolda@vp.pl>
2021-10-31 15:16:15 +0100

    refactor: use Section component

--- cashly_web/src/pages/Spendings.js
+++ cashly_web/src/pages/Spendings.js
@@ -1,24 +1,5 @@
 import styled from "styled-components";
-import { Layout, Button, ExpenseList } from "../components";
-
-const StyledHeader = styled.div`
-    width: 100%;
-    display: flex;
-    justify-content: space-between;
-`;
-
-const StyledTextWrapper = styled.div`
-    display: grid;
-    max-width: 600px;
-    margin-right: 1rem;
-    grid-row-gap: 1rem;
-    grid-template-rows: min-content min-content;
-`;
-
-const StyledDescription = styled.p`
-    color: ${({ theme }) => theme.colors.gray600};
-    font-weight: ${({ theme }) => theme.font.weights.semiBold};
-`;
+import { Layout, Button, ExpenseList, Section } from "../components";
 
 const StyledButton = styled(Button)`
     height: min-content;
@@ -53,18 +34,17 @@ export default function Spendings() {
 
     return (
         <Layout>
-            <StyledHeader>
-                <StyledTextWrapper>
-                    <h2>Lista wydatków</h2>
-                    <StyledDescription>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                        Fusce dui nulla, facilisis eu imperdiet non, rhoncus quis nibh. 
-                        Praesent rutrum viverra iaculis. 
-                        Phasellus commodo orci vitae venenatis consequat. 
-                    </StyledDescription>
-                </StyledTextWrapper>
+            <Section
+                title="Lista wydatków"
+                description={`
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Fusce dui nulla, facilisis eu imperdiet non, rhoncus quis nibh. 
+                    Praesent rutrum viverra iaculis. 
+                    Phasellus commodo orci vitae venenatis consequat.
+                `}
+            >
                 <StyledButton variant="primaryOutline">Dodaj wydatek</StyledButton>
-            </StyledHeader>
+            </Section>
             <StyledWrapper>
                 <ExpenseList data={fakeData} />
             </StyledWrapper>
