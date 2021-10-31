b28be579743c09e0dc0f13d7ac5445eb22f35d26
Michał <michalgolda@vp.pl>
2021-10-31 15:16:43 +0100

    feat: add CategoryList component

--- cashly_web/src/pages/SpendCategories.js
+++ cashly_web/src/pages/SpendCategories.js
@@ -1,15 +1,43 @@
 import styled from "styled-components";
-import { Layout } from "../components";
+import { Layout, Section, Button, CategoryList } from "../components";
 
-const StyledText = styled.h1`
-    padding: 2rem;
-    font-size: ${({ theme }) => theme.font.sizes.h1};
+const StyledButton = styled(Button)`
+    width: fit-content;
+    height: min-content;
 `;
 
+const StyledWrapper = styled.div`margin: 2rem 0 2rem 0;`;
+
 export default function SpendCategories() {
+    const fakeData = [
+        {
+            id: "1",
+            name: "test1",
+            color: "#f00"
+        },
+        {
+            id: "2",
+            name: "test2",
+            color: "#f00"
+        }
+    ];
+
     return (
         <Layout>
-            <StyledText>Spend categories</StyledText>
+            <Section
+                title="Lista kategorii"
+                description={`
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Fusce dui nulla, facilisis eu imperdiet non, rhoncus quis nibh. 
+                    Praesent rutrum viverra iaculis. 
+                    Phasellus commodo orci vitae venenatis consequat.
+                `}
+            >
+                <StyledButton variant="primaryOutline">Dodaj kategorie</StyledButton>
+            </Section>
+            <StyledWrapper>
+                <CategoryList data={fakeData} />
+            </StyledWrapper>
         </Layout>
     );
 }
