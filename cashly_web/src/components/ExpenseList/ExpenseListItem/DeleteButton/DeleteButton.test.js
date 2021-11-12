358ac64e26c5542279351a2cb9df706ddc41f67f
Michał <michalgolda@vp.pl>
2021-11-12 23:21:08 +0100

    refactor: remove unused ExpenseList component

--- cashly_web/src/components/ExpenseList/ExpenseListItem/DeleteButton/DeleteButton.test.js
+++
@@ -1,9 +0,0 @@
-import { render } from "@testing-library/react";
-import withTheme from "../../../../utilities/withTheme";
-import DeleteButton from "./DeleteButton";
-
-test("renders DeleteButton component", () => {
-    const { container } = render(withTheme(DeleteButton));
-
-    expect(container.firstChild).toMatchSnapshot();
-})
--- cashly_web/src/components/ExpenseList/ExpenseListItemLoader/ExpenseListItemLoader.test.js
+++
@@ -1,7 +0,0 @@
-import { render } from "@testing-library/react";
-import withTheme from "../../../utilities/withTheme";
-import ExpenseListItemLoader from "./ExpenseListItemLoader";
-
-test("renders ExpenseListItemLoader component", () => {
-    render(withTheme(ExpenseListItemLoader));
-})
