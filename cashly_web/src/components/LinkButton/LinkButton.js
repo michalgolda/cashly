18d75bd3e8f734f3aca212e566df3c978959d7ac
Michał <michalgolda@vp.pl>
2021-11-12 22:47:38 +0100

    refactor: create StyedLink and add two lines space between code and import statements

--- cashly_web/src/components/LinkButton/LinkButton.js
+++ cashly_web/src/components/LinkButton/LinkButton.js
@@ -1,14 +1,19 @@
 import PropTypes from "prop-types";
+import styled from "styled-components";
 import { Link } from "react-router-dom";
+
 import { Button } from "../../components";
+
+
+const StyledLink = styled(Link)`text-decoration: none;`;
 
 function LinkButton(props) {
     const { to, children } = props;
 
     return (
-        <Link to={to}>
+        <StyledLink to={to}>
             <Button {...props}>{children}</Button>
-        </Link>
+        </StyledLink>
     );
 }
 
