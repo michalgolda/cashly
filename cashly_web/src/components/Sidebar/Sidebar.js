4e63a712c605430257a6a6df5dcc7955ad4d35d2
Michał <michalgolda@vp.pl>
2021-11-12 22:52:30 +0100

    feat: redesign Sidebar component
    add app name
    
    add footer and copyright statement

--- cashly_web/src/components/Sidebar/Sidebar.js
+++ cashly_web/src/components/Sidebar/Sidebar.js
@@ -1,23 +1,49 @@
 import styled from "styled-components";
-import NavButton from "./NavButton/NavButton";
 
-const StyledContainer = styled.nav`
+import NavLinkButton from "./NavLinkButton/NavLinkButton";
+
+
+const StyledNav = styled.nav`
+    width: 256px;
+    height: 100vh;
+    padding: 32px;
+    display: flex;
+    position: fixed;
+    flex-direction: column;
+    background-color: white;
+    border-right: 2px solid ${({ theme }) => theme.colors.gray400};
+`;
+
+const StyledAppName = styled.h2`
+    font-weight: ${({ theme }) => theme.fontWeights.extraBold};
+`;
+
+const StyledNavLinkList = styled.ul`
     display: grid;
-    max-width: 300px;
-    min-width: 256px;
-    grid-row-gap: 1rem;
-    height: calc(100vh - 73px);
-    grid-auto-rows: min-content;
-    padding: 1rem 2rem 1rem 2rem;
-    border-right: 1px solid ${({ theme }) => theme.colors.gray500};
+    row-gap: 16px;
+    margin-top: 32px;
+    grid-auto-rows: auto;
+`;
+
+const StyledFooter = styled.footer`
+    margin-top: auto;
+    color: ${({ theme }) => theme.colors.gray600};
+    font-size: ${({ theme }) => theme.fontSizes.h6};
 `;
 
 export default function Sidebar() {
     return (
-        <StyledContainer>
-            <NavButton to="/expenses">Wydatki</NavButton>
-            <NavButton to="/categories">Kategorie wydatków</NavButton>
-        </StyledContainer>
+        <StyledNav>
+            <StyledAppName>Cashly</StyledAppName>
+            <StyledNavLinkList>
+                <NavLinkButton to="/expenses">Wydatki</NavLinkButton>
+                <NavLinkButton to="/categories">Kategorie</NavLinkButton>
+            </StyledNavLinkList>
+            <StyledFooter>
+                <strong>© 2021 Cashly app</strong> <br />
+                by Michał Gołda
+            </StyledFooter>
+        </StyledNav>
     );
 }
 
