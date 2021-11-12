9238637cab8755935dc0701666d6fec562927b16
Michał <michalgolda@vp.pl>
2021-11-12 23:14:50 +0100

    refactor: improve structure and readability code of createExpenseCategory mutation function

--- cashly_web/src/mutations.js
+++ cashly_web/src/mutations.js
@@ -1,6 +1,14 @@
 import axios from "axios";
 import config from "./config";
 
+
+export async function createExpenseCategory(data) {
+	const requestURL = `${config.apiURL}/spend-categories/`;
+	const response = await axios.post(requestURL, data);
+
+	return response;
+}
+
 export async function createExpenseCategory(newExpenseCategoryData) {
 	const response = await axios.post(
 		`${config.apiURL}/spend-categories/`, 
