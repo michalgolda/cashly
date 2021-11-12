e16d7032ff548b4f384ced14ea18c455cad9f7e1
Michał <michalgolda@vp.pl>
2021-11-12 23:21:08 +0100

    refactor: remove unused ExpenseList component

--- cashly_web/src/components/ExpenseList/ExpenseListItem/EditButton/EditButton.test.js
+++
@@ -1,9 +0,0 @@
-import { render } from "@testing-library/react";
-import withTheme from "../../../../utilities/withTheme";
-import EditButton from "./EditButton";
-
-test("renders EditButton component", () => {
-    const { container } = render(withTheme(EditButton));
-
-    expect(container.firstChild).toMatchSnapshot();
-})
