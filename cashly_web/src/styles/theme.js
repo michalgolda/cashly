592c000b103af72a61ac23b111dc91527a0bb305
Michał <michalgolda@vp.pl>
2021-11-12 23:00:47 +0100

    feat: change colors

--- cashly_web/src/styles/theme.js
+++ cashly_web/src/styles/theme.js
@@ -1,29 +1,29 @@
 const theme = {
     colors: {
-        primary500: "#212529",
-        primary400: "#1e2125",
-        primary300: "#373b3e",
+        primary500: "#2f2f2f",
+        primary400: "#2f3237",
+        primary300: "#4d4d4d",
         gray600: "#a5a5a5",
-        gray500: "#ebebeb",
         gray400: "#eeeeee",
         gray300: "#f0f0f0",
-        blue500: "#4361ee",
-        blue400: "#5671f0", 
-        blue300: "#7b90f3",
-        red500: "#e6004c",
-        red400: "#ff0054",
-        red300: "#ff1a65",
-        white: "#fff"
+        gray200: "#f5f5f5",
+        gray100: "#f9f9f9",
+        blue500: "#4f29e6",
+        blue400: "#582eff", 
+        blue300: "#7958ff",
+        red500: "#e60053",
+        red400: "#ff005c",
+        red300: "#ff337d"
     },
     fontFamily: "'Sora', sans-serif",
     fontSizes: {
-            h1: "2.488rem",
-            h2: "2.074rem",
-            h3: "1.728rem",
-            h4: "1.44rem",
-            h5: "1.2rem",
-            h6: ".833rem"
-        },
+        h1: "2.488rem",
+        h2: "2.074rem",
+        h3: "1.728rem",
+        h4: "1.44rem",
+        h5: "1.2rem",
+        h6: ".833rem"
+    },
     fontWeights: {
         regular: 400,
         semiBold: 600,
