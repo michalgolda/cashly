0cb9301c08944c93c9b4099118787b97dda9b460
Michał <michalgolda@vp.pl>
2021-11-12 22:50:09 +0100

    feat: add NavLinkButton subcomponent of Sidebar component

---
+++ cashly_web/src/components/Sidebar/NavLinkButton/NavLinkButton.js
@@ -0,0 +1,45 @@
+import PropTypes from "prop-types";
+import styled, { css } from "styled-components";
+import { NavLink, useRouteMatch } from "react-router-dom";
+
+import { Button } from "../../../components";
+
+
+const StyledNavLink = styled(NavLink)`text-decoration: none;`;
+
+const StyledNavLinkButton = styled(Button)`
+    justify-content: left;
+    color: ${({ theme }) => theme.colors.gray600};
+    font-weight: ${({ theme }) => theme.fontWeights.semiBold};
+
+    ${({ isActive }) => isActive && css`
+        color: ${({ theme }) => theme.colors.primary400};
+        background-color: ${({ theme }) => theme.colors.gray300};
+
+        &:hover { background-color: ${({ theme }) => theme.colors.gray300}; }
+        &:active { background-color: ${({ theme }) => theme.colors.gray300}; }
+    `}
+`;
+
+function NavLinkButton({ children, to }) {
+    const isActive = useRouteMatch({ path: to });
+
+    return (
+        <StyledNavLink to={to}>
+            <StyledNavLinkButton
+                variant="text"
+                isActive={isActive}
+                fullWidth
+            >
+                {children}
+            </StyledNavLinkButton>
+        </StyledNavLink>
+    );
+}
+
+NavLinkButton.propTypes = { 
+    to: PropTypes.string.isRequired,
+    children: PropTypes.node.isRequired
+};
+
+export default NavLinkButton;
