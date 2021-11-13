e451d1c27af07768e7cafd975aa3761101120765
Michał <michalgolda@vp.pl>
2021-11-13 02:08:01 +0100

    feat: add ExpenseListSkeleton component

---
+++ cashly_web/src/pages/Expenses/ExpenseListSkeleton/ExpenseListSkeleton.js
@@ -0,0 +1,12 @@
+import { Skeleton } from "../../../components";
+
+
+export default function CategoryListSkeleton() {
+    return (
+        <>
+            <Skeleton height={70} />
+            <Skeleton height={70} />
+            <Skeleton height={70} />
+        </>
+    )
+}
