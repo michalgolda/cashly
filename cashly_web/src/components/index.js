0aba6f61446785644f5e4ba28edf2d74930bd428
Michał <michalgolda@vp.pl>
2021-11-13 01:07:35 +0100

    feat: add List component

--- cashly_web/src/components/index.js
+++ cashly_web/src/components/index.js
@@ -1,32 +1,29 @@
+import List from "./List/List";
 import Page from "./Page/Page";
-import Modal from "./Modal/Modal";
 import Input from "./Input/Input";
-import Error from "./Error/Error";
-import Header from "./Header/Header";
-import Button from "./Button/Button";
+import Modal from "./Modal/Modal";
 import Layout from "./Layout/Layout";
-import Router from "./Router/Router";
+import Button from "./Button/Button";
+import Header from "./Header/Header";
 import Sidebar from "./Sidebar/Sidebar";
 import ListItem from "./ListItem/ListItem";
 import Informer from "./Informer/Informer";
 import Skeleton from "./Skeleton/Skeleton";
 import IconButton from "./IconButton/IconButton";
 import LinkButton from "./LinkButton/LinkButton";
-import ExpenseList from "./ExpenseList/ExpenseList";
-import CategoryList from "./CategoryList/CategoryList";
 
 export { 
     Page,
+    List,
     Input,
     Modal,
-    Error,
-    Header, 
-    Button, 
     Layout,
-    Router,
-    Section,
+    Button, 
+    Header, 
     Sidebar,
     Informer, 
     Skeleton,
     ListItem,
     IconButton,
+    LinkButton
+}
