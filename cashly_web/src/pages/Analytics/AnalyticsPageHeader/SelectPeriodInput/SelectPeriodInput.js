c8eac11d559fcd7e1c8a9df6a6018e17e38b13ec
Michał <michalgolda@vp.pl>
2021-11-30 20:19:21 +0100

    feat: add SelectPeriodInput component

---
+++ cashly_web/src/pages/Analytics/AnalyticsPageHeader/SelectPeriodInput/SelectPeriodInput.js
@@ -0,0 +1,10 @@
+import { Input } from "../../../../components";
+
+
+export default function SelectPeriodInput() {
+    return (
+        <Input as="select">
+            <option>Ostatni tydzień</option>
+        </Input>
+    );
+}
