16b0f0977d2f35387879a80e8ff3e0c61f063df8
Michał <michalgolda@vp.pl>
2021-12-08 15:48:30 +0100

    feat: add get_all_by method to Repository abstract class and implement it each of particular implementation of repository

--- cashly_api/app/repositories.py
+++ cashly_api/app/repositories.py
@@ -6,144 +6,159 @@ from app.database import Database
 
 
 class Repository(ABC):
-    def __init__(self, db: Database):
-        self._db = db
+	def __init__(self, db: Database):
+		self._db = db
 
-    @abstractmethod
-    def add():
-        raise NotImplementedError
+	@abstractmethod
+	def add():
+		raise NotImplementedError
 
-    @abstractmethod
-    def get():
-        raise NotImplementedError
+	@abstractmethod
+	def get():
+		raise NotImplementedError
 
-    @abstractmethod
-    def get_one_by():
-        raise NotImplementedError
+	@abstractmethod
+	def get_one_by():
+		raise NotImplementedError
 
-    @abstractmethod
-    def list():
-        raise NotImplementedError
+	@abstractmethod
+	def get_all_by():
+		raise NotImplementedError
 
-    @abstractmethod
-    def delete():
-        raise NotImplementedError
+	@abstractmethod
+	def list():
+		raise NotImplementedError
 
-    @abstractmethod
-    def update():
-        raise NotImplementedError
+	@abstractmethod
+	def delete():
+		raise NotImplementedError
+
+	@abstractmethod
+	def update():
+		raise NotImplementedError
 
 
 class ExpenseRepository(Repository):
-    def add(self, expense: schemas.ExpenseCreate,
-            expense_category: models.ExpenseCategory) -> models.Expense:
-        with self._db.session_factory() as session:
-            new_expense = models.Expense(
-                expense_category, 
-                amount=expense.amount,
-                realised_date=expense.realised_date
-            )
+	def add(self, expense: schemas.ExpenseCreate,
+			expense_category: models.ExpenseCategory) -> models.Expense:
+		with self._db.session_factory() as session:
+			new_expense = models.Expense(
+				expense_category, 
+				amount=expense.amount,
+				realised_date=expense.realised_date
+			)
 
-            session.add(new_expense)
-            session.commit()
-            session.refresh(new_expense)
+			session.add(new_expense)
+			session.commit()
+			session.refresh(new_expense)
 
-            return new_expense
+			return new_expense
 
-    def get(self, expense_id: str) -> typing.Union[models.Expense, None]:
-        with self._db.session_factory() as session:
-            expense = session.query(models.Expense).get(expense_id)
+	def get(self, expense_id: str) -> typing.Union[models.Expense, None]:
+		with self._db.session_factory() as session:
+			expense = session.query(models.Expense).get(expense_id)
 
-            return expense
+			return expense
 
-    def get_one_by(self, *args, **kwargs) -> typing.Union[models.Expense, None]:
-        with self._db.session_factory() as session:
-            expense = session.query(models.Expense) \
-                .filter_by(*args, **kwargs).first()
+	def get_one_by(self, **kwargs) -> typing.Union[models.Expense, None]:
+		with self._db.session_factory() as session:
+			expense = session.query(models.Expense) \
+				.filter_by(**kwargs).first()
 
-            return expense
+			return expense
 
-    def list(self) -> typing.List[models.Expense]:
-        with self._db.session_factory() as session:
-            expenses = session.query(models.Expense).all()
+	def get_all_by(self, **kwargs) -> typing.List[models.Expense]:
+		with self._db.session_factory() as session:
+			expenses = session.query(models.Expense).filter_by(**kwargs).all()
 
-            return expenses
+			return expenses
 
-    def delete(self, expense: models.Expense) -> models.Expense:
-        with self._db.session_factory() as session:
-            session.delete(expense)
-            session.commit()
+	def list(self) -> typing.List[models.Expense]:
+		with self._db.session_factory() as session:
+			expenses = session.query(models.Expense).all()
 
-            return expense
+			return expenses
 
-    def update(self, expense: models.Expense, expense_category: models.ExpenseCategory, expense_update: schemas.ExpenseUpdate) -> models.Expense:
-        with self._db.session_factory() as session:
-            expense = session.query(models.Expense).get(expense.id)
-            
-            if expense_category: 
-                expense_category = session.query(models.ExpenseCategory).get(expense_category.id)
+	def delete(self, expense: models.Expense) -> models.Expense:
+		with self._db.session_factory() as session:
+			session.delete(expense)
+			session.commit()
 
-            expense.amount = expense_update.amount
-            expense.expense_category = expense_category
-            expense.realised_date = expense_update.realised_date
+			return expense
 
-            session.commit()
-            session.refresh(expense)
+	def update(self, expense: models.Expense, expense_category: models.ExpenseCategory, expense_update: schemas.ExpenseUpdate) -> models.Expense:
+		with self._db.session_factory() as session:
+			expense = session.query(models.Expense).get(expense.id)
+			
+			if expense_category: 
+				expense_category = session.query(models.ExpenseCategory).get(expense_category.id)
 
-        return expense
+			expense.amount = expense_update.amount
+			expense.expense_category = expense_category
+			expense.realised_date = expense_update.realised_date
+
+			session.commit()
+			session.refresh(expense)
+
+		return expense
 
 
 class ExpenseCategoryRepository(Repository):
-    def add(self, expense_category: schemas.ExpenseCategoryCreate
-            ) -> models.ExpenseCategory:
-        with self._db.session_factory() as session:
-            new_expense_category = models.ExpenseCategory(
-                name=expense_category.name, color=expense_category.color)
+	def add(self, expense_category: schemas.ExpenseCategoryCreate
+			) -> models.ExpenseCategory:
+		with self._db.session_factory() as session:
+			new_expense_category = models.ExpenseCategory(
+				name=expense_category.name, color=expense_category.color)
 
-            session.add(new_expense_category)
-            session.commit()
-            session.refresh(new_expense_category)
+			session.add(new_expense_category)
+			session.commit()
+			session.refresh(new_expense_category)
 
-            return new_expense_category
+			return new_expense_category
 
-    def get(self, expense_category_id: str
-            ) -> typing.Union[models.ExpenseCategory, None]:
-        with self._db.session_factory() as session:
-            expense_category = session.query(models.ExpenseCategory) \
-                .get(expense_category_id)
+	def get(self, expense_category_id: str
+			) -> typing.Union[models.ExpenseCategory, None]:
+		with self._db.session_factory() as session:
+			expense_category = session.query(models.ExpenseCategory) \
+				.get(expense_category_id)
 
-            return expense_category
+			return expense_category
 
-    def get_one_by(self, *args,
-                   **kwargs) -> typing.Union[models.ExpenseCategory, None]:
-        with self._db.session_factory() as session:
-            expense_category = session.query(models.ExpenseCategory) \
-                .filter_by(*args, **kwargs).first()
+	def get_one_by(self, **kwargs) -> typing.Union[models.ExpenseCategory, None]:
+		with self._db.session_factory() as session:
+			expense_category = session.query(models.ExpenseCategory) \
+				.filter_by(**kwargs).first()
 
-            return expense_category
+			return expense_category
 
-    def list(self) -> typing.List[models.ExpenseCategory]:
-        with self._db.session_factory() as session:
-            expense_categories = session.query(models.ExpenseCategory).all()
+	def get_all_by(self, **kwargs) -> typing.List[models.ExpenseCategory]:
+		with self._db.session_factory() as session:
+			expense_categories = session.query(models.ExpenseCategory).filter_by(**kwargs).all()
 
-            return expense_categories
+			return expense_categories
 
-    def delete(self,
-               expense_category: models.ExpenseCategory) -> models.ExpenseCategory:
-        with self._db.session_factory() as session:
-            session.delete(expense_category)
-            session.commit()
+	def list(self) -> typing.List[models.ExpenseCategory]:
+		with self._db.session_factory() as session:
+			expense_categories = session.query(models.ExpenseCategory).all()
 
-        return expense_category
+			return expense_categories
 
-    def update(self, expense_category: models.ExpenseCategory, expense_category_update: schemas.ExpenseCategoryUpdate) -> models.ExpenseCategory:
-        with self._db.session_factory() as session:
-            expense_category = session.query(models.ExpenseCategory).get(expense_category.id)
+	def delete(self,
+			   expense_category: models.ExpenseCategory) -> models.ExpenseCategory:
+		with self._db.session_factory() as session:
+			session.delete(expense_category)
+			session.commit()
 
-            expense_category.name = expense_category_update.name
-            expense_category.color = expense_category_update.color
+		return expense_category
 
-            session.commit()
-            session.refresh(expense_category)
+	def update(self, expense_category: models.ExpenseCategory, expense_category_update: schemas.ExpenseCategoryUpdate) -> models.ExpenseCategory:
+		with self._db.session_factory() as session:
+			expense_category = session.query(models.ExpenseCategory).get(expense_category.id)
 
-        return expense_category
+			expense_category.name = expense_category_update.name
+			expense_category.color = expense_category_update.color
+
+			session.commit()
+			session.refresh(expense_category)
+
+		return expense_category
