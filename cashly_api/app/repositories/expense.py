8f0b711c9dd5a711efca3165989d2e5780b00ef0
Michał <michalgolda@vp.pl>
2021-12-20 22:47:03 +0100

    feat: add SQLAlchemyExpenseRepository class

--- cashly_api/app/repositories/expense.py
+++ cashly_api/app/repositories/expense.py
@@ -2,6 +2,8 @@ from uuid import UUID
 from abc import ABC, abstractmethod
 from typing import Union, List, NoReturn
 
+from sqlalchemy.orm.session import Session
+
 from app.entities import Expense
 
 
@@ -25,3 +27,36 @@ class AbstractExpenseRepository(ABC):
     @abstractmethod
     def delete(self, expense: Expense) -> NoReturn:
         pass
+
+
+class SQLAlchemyExpenseRepository(AbstractExpenseRepository):
+    def __init__(self, session: Session):
+        self._session = session
+
+    def get_by_id(self, expense_id: UUID) -> Union[Expense, None]:
+        expense = self._session.query(Expense) \
+            .filter_by(id=expense_id) \
+            .first()
+
+        return expense
+
+    def get_all(self) -> List[Expense]:
+        expenses = self._session.query(Expense).all()
+
+        return expenses
+
+    def add(self, expense: Expense) -> Expense:
+        self._session.add(expense)
+        self._session.commit()
+
+        return expense
+
+    def save(self, expense: Expense) -> Expense:
+        self._session.commit()
+        self._session.refresh(expense)
+
+        return expense
+
+    def delete(self, expense: Expense) -> NoReturn:
+        self._session.delete(expense)
+        self._session.commit()
