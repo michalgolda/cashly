054c1a787c3ef4604c19d1141cfc8d1e72814eef
Michał <michalgolda@vp.pl>
2021-12-20 15:16:28 +0100

    feat: add Expense entity

---
+++ cashly_api/app/entities/expense.py
@@ -0,0 +1,16 @@
+from uuid import UUID
+from typing import Optional
+from dataclasses import dataclass
+from datetime import date, datetime
+
+from app.entities.expense_category import ExpenseCategory
+
+
+@dataclass
+class Expense:
+    amount: float
+    realised_date: date
+    id: Optional[UUID] = None
+    category: Optional[ExpenseCategory] = None
+    created_at: Optional[datetime] = None
+    updated_at: Optional[datetime] = None
