805422edeb65a2c49dc20fba8c7b30c2725cf6b6
Michał <michalgolda@vp.pl>
2021-12-20 22:47:41 +0100

    feat: add import statement of SQLAlchemyExpenseRepository, SQLAlchemyExpenseCategoryRepository lcasses

--- cashly_api/app/repositories/__init__.py
+++ cashly_api/app/repositories/__init__.py
@@ -1,2 +1,2 @@
-from .expense import AbstractExpenseRepository
-from .expense_category import AbstractExpenseCategoryRepository
+from .expense import AbstractExpenseRepository, SQLAlchemyExpenseRepository
+from .expense_category import AbstractExpenseCategoryRepository, SQLAlchemyExpenseCategoryRepository
