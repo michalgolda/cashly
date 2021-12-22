f72885aa7af59044810cdd1f173cad34388a4820
Michał <michalgolda@vp.pl>
2021-12-22 20:37:02 +0100

    refactor: change some issues about new api architecture

--- cashly_web/src/mutations.js
+++ cashly_web/src/mutations.js
@@ -3,21 +3,21 @@ import config from "./config";
 
 
 export async function createExpenseCategory(data) {
-	const requestURL = `${config.apiURL}/expense-categories/`;
+	const requestURL = `${config.apiURL}/expense_categories/`;
 	const response = await axios.post(requestURL, data);
 
 	return response;
 }
 
 export async function deleteExpenseCategory(expenseCategoryId) {
-	const requestURL = `${config.apiURL}/expense-categories/${expenseCategoryId}/`;
+	const requestURL = `${config.apiURL}/expense_categories/${expenseCategoryId}/`;
 	const response = await axios.delete(requestURL);
 
 	return response;
 }
 
 export async function updateExpenseCategory({ expenseCategoryId, data }) {
-	const requestURL = `${config.apiURL}/expense-categories/${expenseCategoryId}/`;
+	const requestURL = `${config.apiURL}/expense_categories/${expenseCategoryId}/`;
 	const response = await axios.put(requestURL, data);
 
 	return response;
