01f03d1a44ace6ea062b978826b523656f28add7
Michał <michalgolda@vp.pl>
2021-12-29 22:21:22 +0100

    feat: add TotalAmountOfExpenses widget

--- cashly_web/src/pages/Analytics/WidgetList/WidgetList.js
+++ cashly_web/src/pages/Analytics/WidgetList/WidgetList.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import GeneralExpensesWidget from "../GeneralExpensesWidget/GeneralExpensesWidget";
 import ExpensesByCategoryWidget from "../ExpensesByCategoryWidget/ExpensesByCategoryWidget";
 import CountExpensesByCategoryWidget from "../CountExpensesByCategoryWidget/CountExpensesByCategoryWidget";
-
+import TotalAmountOfExpensesWidget from "../TotalAmountOfExpensesWidget/TotalAmountOfExpensesWidget";
 
 const StyledList = styled.div`
     display: grid;
@@ -11,10 +11,18 @@ const StyledList = styled.div`
     grid-template-rows: auto auto;
 `;
 
+const StyledGroup = styled.div`
+    display: grid;
+    grid-column-gap: 32px;
+    grid-template-columns: ${({ columns }) => `repeat(${columns}, 1fr)`};
+`;
 
 export default function WidgetList() {
     return (
         <StyledList>
+            <StyledGroup columns={1}>
+                <TotalAmountOfExpensesWidget />
+            </StyledGroup>
             <GeneralExpensesWidget />
             <ExpensesByCategoryWidget />
             <CountExpensesByCategoryWidget />
