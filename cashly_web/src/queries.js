a012dfe5326838bdadd4a7abe163d0314717a55d
Michał <michalgolda@vp.pl>
2021-12-29 14:49:48 +0100

    refactor: remove data variable which is redundant

--- cashly_web/src/queries.js
+++ cashly_web/src/queries.js
@@ -7,15 +7,14 @@ import { buildURL } from "./utilities/url";
 export async function getAllExpenseCategories() {
     const requestURL = `${config.apiURL}/expense_categories/`;
     const response = await axios.get(requestURL);
-    const data = await response.data;
-
-    return data;
+    return await response.data;
 }
 
 export async function getAllExpenses() {
     const requestURL = `${config.apiURL}/expenses/`;
     const response = await axios.get(requestURL);
-    const data = await response.data;
+    return await response.data;
+}
 
 export async function getAnalyticsData(analyticsName, queryParams) {
     const baseURLString = `${config.apiURL}/analytics/${analyticsName}/`;
