bb61a63db209a98b4635f3789d85b9d8a5d4663f
Michał <michalgolda@vp.pl>
2021-12-29 16:08:05 +0100

    feat: set list width as 100%

--- cashly_web/src/components/List/List.js
+++ cashly_web/src/components/List/List.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 
 
 const StyledList = styled.ul`
+    width: 100%;
     display: grid;
     row-gap: 16px;
     list-style: none;
@@ -11,8 +12,8 @@ const StyledList = styled.ul`
 function List(props) {
     const { children } = props;
 
-    return <StyledList {...props}>{children}</StyledList>
-};
+    return <StyledList {...props}>{children}</StyledList>;
+}
 
 List.propTypes = { children: PropTypes.element };
 
