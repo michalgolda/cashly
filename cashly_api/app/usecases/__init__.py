6c180fe4a91c860ff956a0f15a6b673061665ad2
michalgolda <michalgolda@vp.pl>
2022-03-09 15:49:17 +0100

    feat: create 'ImportExpenseUseCase' class

--- cashly_api/app/usecases/__init__.py
+++ cashly_api/app/usecases/__init__.py
@@ -1,33 +1,26 @@
 from .expense import (
     GetAllExpensesUseCase,
     GetAllExpensesResult,
-
     GetExpenseByIdUseCase,
     GetExpenseByIdRequest,
-
     CreateExpenseUseCase,
     CreateExpenseRequest,
-
     DeleteExpenseUseCase,
     DeleteExpenseRequest,
-
     UpdateExpenseUseCase,
     UpdateExpenseRequest,
-
-    ExportExpensesUseCase
+    ExportExpensesUseCase,
+    ImportExpensesRequest,
+    ImportExpensesUseCase
 )
 from .expense_category import (
     GetAllExpenseCategoriesUseCase,
-
     GetExpenseCategoryByIdUseCase,
     GetExpenseCategoryByIdRequest,
-
     CreateExpenseCategoryUseCase,
     CreateExpenseCategoryRequest,
-
     DeleteExpenseCategoryUseCase,
     DeleteExpenseCategoryRequest,
-
     UpdateExpenseCategoryUseCase,
     UpdateExpenseCategoryRequest
 )
