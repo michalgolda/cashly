56ac4cd08e58172895d8ce9fcb1cada8037d077d
michalgolda <michal.golda@hotmail.com>
2022-06-11 23:42:53 +0200

    feat: create 'User' entity

--- cashly_api/app/entities/user.py
+++ cashly_api/app/entities/user.py
@@ -1,14 +1,14 @@
+import bcrypt
 from uuid import UUID
 from typing import Optional
 from datetime import datetime
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 
 @dataclass
 class User:
-    first_name: str
-    last_name: str
     email: str
+    password: str
     id: Optional[UUID] = None
     created_at: Optional[datetime] = None
     updated_at: Optional[datetime] = None
