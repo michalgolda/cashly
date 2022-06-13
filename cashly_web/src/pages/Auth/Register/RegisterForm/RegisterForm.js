38c601bad621886e375f4e71f76af330fec3d590
michalgolda <michal.golda@hotmail.com>
2022-06-13 09:08:49 +0100

    feat: use 'register' mutation witch register form

--- cashly_web/src/pages/Auth/Register/RegisterForm/RegisterForm.js
+++ cashly_web/src/pages/Auth/Register/RegisterForm/RegisterForm.js
@@ -1,8 +1,11 @@
 import * as yup from "yup";
 import { useFormik } from "formik";
+import { useMutation } from "react-query";
 
+import { register } from "../../../../mutations";
 import AuthForm from "../../AuthForm/AuthForm";
 import { Input, Button } from "../../../../components";
+import { useHistory } from "react-router";
 
 export default function RegisterForm() {
     const initialValues = {
@@ -10,6 +13,7 @@ export default function RegisterForm() {
         password: "",
         confirmPassword: ""
     };
+
     const validationSchema = yup.object({
         email: yup.string()
             .email("Podaj prawidłowy email")
@@ -23,10 +27,37 @@ export default function RegisterForm() {
             )
             .required("To pole jest wymagane")
     });
+    
+    const history = useHistory();
+
+    const mutation = useMutation(register, {
+        onSuccess: () => history.push('/login')
+    });
+
+    const onSubmit = (values, { setFieldError}) => {
+        delete values['confirmPassword'];
+
+        mutation.mutate(values, {
+            onError: ({ response }) => {
+                if (response) {
+                    if (response.status == 400) {
+                        const { code, message } = response.data;
+
+                        switch (code) {
+                            case "UserEmailAlreadyUsed":
+                                setFieldError("email", message);
+                                break;
+                        }
+                    }
+                }
+            }
+        });
+    };
+
     const formik = useFormik({
         initialValues,
         validationSchema,
-        onSubmit: (values) => console.log(values)
+        onSubmit
     });
 
     return (
