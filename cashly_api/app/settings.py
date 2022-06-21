e94cc0543bb3e086b720794792f60b6e59fd6594
michalgolda <michal.golda@hotmail.com>
2022-06-22 00:24:36 +0200

    feat: add 'ACCESS_TOKEN_ALGORITHM' and 'ACCESS_TOKEN_EXPIRATION' settings properties

--- cashly_api/app/settings.py
+++ cashly_api/app/settings.py
@@ -4,5 +4,8 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     DATABASE_URL: str = 'sqlite:///database.sqlite'
     SECRET_KEY: str = 'secret key'
+    
+    ACCESS_TOKEN_ALGORITHM: str = 'HS256'
+    ACCESS_TOKEN_EXPIRATION: int = 1800
 
 settings = Settings()
