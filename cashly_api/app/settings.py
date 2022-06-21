dedc8186c26406142b2e428575ed6c6469cd7e04
michalgolda <michal.golda@hotmail.com>
2022-06-22 00:07:50 +0200

    feat: add 'SECRET_KEY' property to settings

--- cashly_api/app/settings.py
+++ cashly_api/app/settings.py
@@ -2,7 +2,7 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = "sqlite:///database.sqlite"
-
+    DATABASE_URL: str = 'sqlite:///database.sqlite'
+    SECRET_KEY: str = 'secret key'
 
 settings = Settings()
