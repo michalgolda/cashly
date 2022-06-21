ec0d7832ed7e922b5e53d001502fbbcd5ac99c28
michalgolda <michal.golda@hotmail.com>
2022-06-21 23:36:36 +0200

    chore: remove useless module

--- cashly_api/app/authentication.py
+++
@@ -1,8 +0,0 @@
-import bcrypt
-
-
-def generate_password_hash(password: str) -> str:
-  return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
-
-def check_password_hash(password: str, hashed_password: str) -> bool:
-  return bcrypt.checkpw(password.encode('utf-8'), hashed_password.encode('utf-8'))
