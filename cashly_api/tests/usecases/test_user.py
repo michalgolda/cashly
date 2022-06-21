f4688b561d14394191c69237a7b80cb02f8c8265
michalgolda <michal.golda@hotmail.com>
2022-06-22 00:03:25 +0200

    refactor: change get user by id usecase name class and input, output class names

--- cashly_api/tests/usecases/test_user.py
+++ cashly_api/tests/usecases/test_user.py
@@ -1,57 +1,32 @@
 import pytest
-
 from app.usecases.user import (
     GetUserByIdUseCase,
-    UserNotFoundError,
-    CreateUserUseCase,
-    UserEmailAlreadyUsedError
+    UserNotFoundError
 )
-from app.entities import User
-
 
 def test_get_user_by_id(mocker):
-    mock_user = mocker.patch('app.entities.User')
-    mock_user_repo = mocker.patch('app.repositories.AbstractUserRepository')
-    mock_user_repo.get_by_id.return_value = mock_user
+    mock_user_entity = mocker.patch('app.entities.User')
+    mock_user_repo = mocker.patch('app.repositories.UserRepository')
+    mock_user_repo.get_by_id.return_value = mock_user_entity
+    
+    mock_get_user_by_id_usecase_input = mocker.patch('app.usecases.user.GetUserByIdUseCaseInput')
 
-    usecase = GetUserByIdUseCase(user_repo=mock_user_repo)
-    mock_usecase_request = mocker.patch('app.usecases.user.GetUserByIdRequest')
-    result = usecase.execute(mock_usecase_request)
+    get_user_by_id_usecase = GetUserByIdUseCase(user_repo=mock_user_repo)
+    get_user_by_id_usecase_result = get_user_by_id_usecase.execute(mock_get_user_by_id_usecase_input)
+    
+    assert get_user_by_id_usecase_result.user == mock_user_entity
 
-    mock_user_repo.get_by_id.assert_called_once_with(mock_usecase_request.user_id)
-
-    assert result.user == mock_user
-
+    mock_user_repo.get_by_id.assert_called_once_with(mock_get_user_by_id_usecase_input.user_id)
 
 def test_get_user_by_id_when_user_not_found(mocker):
-    mock_user_repo = mocker.patch('app.repositories.AbstractUserRepository')
+    mock_user_repo = mocker.patch('app.repositories.UserRepository')
     mock_user_repo.get_by_id.return_value = None
+    
+    mock_get_user_by_id_usecase_input = mocker.patch('app.usecases.user.GetUserByIdUseCaseInput')
 
-    usecase = GetUserByIdUseCase(user_repo=mock_user_repo)
-    mock_usecase_request = mocker.patch('app.usecases.user.GetUserByIdRequest')
-
+    get_user_by_id_usecase = GetUserByIdUseCase(user_repo=mock_user_repo)
     with pytest.raises(UserNotFoundError):
-        usecase.execute(mock_usecase_request)
+        get_user_by_id_usecase.execute(mock_get_user_by_id_usecase_input)
 
-    mock_user_repo.get_by_id.assert_called_once_with(mock_usecase_request.user_id)
-
-
-def test_create_user(mocker):
-    mock_user_repo = mocker.patch('app.repositories.user.AbstractUserRepository')
-    mock_user_repo.get_by_email.return_value = None
-
-    mock_usecase_request = mocker.patch('app.usecases.user.CreateUserRequest')
-    create_user_usecase = CreateUserUseCase(user_repo=mock_user_repo)
-    result = create_user_usecase.execute(mock_usecase_request)
-
-    mock_user_repo.get_by_email.assert_called_once_with(mock_usecase_request.email)
-
-    mock_user_repo.add.assert_called_once_with(
-        User(
-            email=mock_usecase_request.email,
-            password=mock_usecase_request.password
-        )
-    )
-
-    assert result.user.email == mock_usecase_request.email
-    assert result.user.password == mock_usecase_request.password
+    mock_user_repo.get_by_id.assert_called_once_with(mock_get_user_by_id_usecase_input.user_id)
+    
