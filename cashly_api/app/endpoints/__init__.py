c1e376c1cedff501461dc0ca410c37ccc483b4b1
michalgolda <michal.golda@hotmail.com>
2022-06-22 00:14:05 +0200

    feat: create '/users/me' endpoint

--- cashly_api/app/endpoints/__init__.py
+++ cashly_api/app/endpoints/__init__.py
@@ -1,1 +1,5 @@
+# from .expense import expense_router
+# from .analytics import analytics_router
+# from .expense_category import expense_category_router
+from .user import user_router
 from .authentication import authentication_router
