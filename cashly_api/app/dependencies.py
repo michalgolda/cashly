e7d36376f32f88f53117fd1fae81a9b73265696f
michalgolda <michal.golda@hotmail.com>
2022-06-22 00:08:24 +0200

    feat: create 'get_security_manager' and 'get_current_user' dependencies

--- cashly_api/app/dependencies.py
+++ cashly_api/app/dependencies.py
@@ -1,18 +1,35 @@
-from app.database import session
-from app.repositories import SQLAlchemyExpenseRepository, SQLAlchemyExpenseCategoryRepository, SQLAlchemyUserRepository
+from fastapi import Depends
 
+from app.entities import User
+from app.database import session
+from app.settings import settings
+from app.security import (
+    AccessTokenPayload, 
+    HTTPAccessToken, 
+    SecurityManager, 
+    DefaultSecurityManager
+)
+from app.repositories import (
+    SQLAlchemyExpenseRepository, 
+    SQLAlchemyExpenseCategoryRepository, 
+    UserRepository,
+    SQLAlchemyUserRepository
+)
 
 def get_expense_repo():
-    repo = SQLAlchemyExpenseRepository(session)
-
-    return repo
-
+    return SQLAlchemyExpenseRepository(session)
 
 def get_expense_category_repo():
-    repo = SQLAlchemyExpenseCategoryRepository(session)
+    return SQLAlchemyExpenseCategoryRepository(session)
 
-    return repo
-
-
-def get_user_repo():
+def get_user_repo() -> UserRepository:
     return SQLAlchemyUserRepository(session)
+
+def get_security_manager() -> SecurityManager:
+    return DefaultSecurityManager(settings.SECRET_KEY)
+
+def get_current_user(
+    user_repo: UserRepository = Depends(get_user_repo),
+    access_token_payload: AccessTokenPayload = Depends(HTTPAccessToken(security_manager=get_security_manager()))
+) -> User:
+    return user_repo.get_by_id(access_token_payload.sub)
