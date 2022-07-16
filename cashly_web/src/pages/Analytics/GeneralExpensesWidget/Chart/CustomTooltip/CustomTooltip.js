2a65a3b0f935e3a10762cf3b92ab4ff638042abf
michalgolda <michal.golda@hotmail.com>
2022-07-16 14:40:31 +0200

    refactor: use 'Intl.NumberFormat' instead of 'currencyjs' library

--- cashly_web/src/pages/Analytics/GeneralExpensesWidget/Chart/CustomTooltip/CustomTooltip.js
+++ cashly_web/src/pages/Analytics/GeneralExpensesWidget/Chart/CustomTooltip/CustomTooltip.js
@@ -1,28 +1,30 @@
 import styled from "styled-components";
-
+import { defaultCurrencyFormat } from "@/utils";
 
 const StyledTooltip = styled.div`
-    padding: 15px;
-    border-radius: 2px;
-    background-color: #fff;
-    border: 1px solid ${({ theme }) => theme.colors.gray400};
+  padding: 15px;
+  border-radius: 2px;
+  background-color: #fff;
+  border: 1px solid ${({ theme }) => theme.colors.gray400};
 `;
 
 const StyledLabel = styled.p`
-    font-size: ${({ theme }) => theme.fontSizes.h5};
-    font-weight: ${({ theme }) => theme.fontWeights.semiBold};
+  font-size: ${({ theme }) => theme.fontSizes.h5};
+  font-weight: ${({ theme }) => theme.fontWeights.semiBold};
 `;
 
 const StyledValue = styled.p`
-    color: ${({ theme }) => theme.colors.blue400};
-    font-size: ${({ theme }) => theme.fontSizes.h5};
+  color: ${({ theme }) => theme.colors.blue400};
+  font-size: ${({ theme }) => theme.fontSizes.h5};
 `;
 
 export default function CustomTooltip({ active, payload, label }) {
-    return !(active && payload && payload.length) ? null : (
-        <StyledTooltip>
-            <StyledLabel>{label}</StyledLabel>
-            <StyledValue>{payload[0].value} PLN</StyledValue>
-        </StyledTooltip>
-    );
+  return !(active && payload && payload.length) ? null : (
+    <StyledTooltip>
+      <StyledLabel>{label}</StyledLabel>
+      <StyledValue>
+        {defaultCurrencyFormat.format(payload[0].value)}
+      </StyledValue>
+    </StyledTooltip>
+  );
 }
