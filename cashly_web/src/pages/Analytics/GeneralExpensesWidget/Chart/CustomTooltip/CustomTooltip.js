a92ea5e12e16cd2d91676433a1240010e5cb3095
michalgolda <michal.golda@hotmail.com>
2022-07-16 15:00:01 +0200

    refactor: remove repeated code by creating new 'Tooltip' component

--- cashly_web/src/pages/Analytics/GeneralExpensesWidget/Chart/CustomTooltip/CustomTooltip.js
+++ cashly_web/src/pages/Analytics/GeneralExpensesWidget/Chart/CustomTooltip/CustomTooltip.js
@@ -1,30 +1,11 @@
-import styled from "styled-components";
 import { defaultCurrencyFormat } from "@/utils";
-
-const StyledTooltip = styled.div`
-  padding: 15px;
-  border-radius: 2px;
-  background-color: #fff;
-  border: 1px solid ${({ theme }) => theme.colors.gray400};
-`;
-
-const StyledLabel = styled.p`
-  font-size: ${({ theme }) => theme.fontSizes.h5};
-  font-weight: ${({ theme }) => theme.fontWeights.semiBold};
-`;
-
-const StyledValue = styled.p`
-  color: ${({ theme }) => theme.colors.blue400};
-  font-size: ${({ theme }) => theme.fontSizes.h5};
-`;
+import Tooltip from "@/pages/Analytics/Tooltip/Tooltip";
 
 export default function CustomTooltip({ active, payload, label }) {
   return !(active && payload && payload.length) ? null : (
-    <StyledTooltip>
-      <StyledLabel>{label}</StyledLabel>
-      <StyledValue>
-        {defaultCurrencyFormat.format(payload[0].value)}
-      </StyledValue>
-    </StyledTooltip>
+    <Tooltip
+      label={label}
+      value={defaultCurrencyFormat.format(payload[0].value)}
+    />
   );
 }
