ff3fb3b326736a71da0c4f72596a6fc217149a19
michalgolda <michal.golda@hotmail.com>
2022-08-09 00:59:44 +0200

    fix: change order of imports

--- cashly_web/src/components/index.js
+++ cashly_web/src/components/index.js
@@ -5,17 +5,17 @@ import Modal from "./Modal/Modal";
 import Layout from "./Layout/Layout";
 import Button from "./Button/Button";
 import Header from "./Header/Header";
-import Sidebar from "./Sidebar/Sidebar";
 import ListItem from "./ListItem/ListItem";
 import Informer from "./Informer/Informer";
 import Skeleton from "./Skeleton/Skeleton";
 import PageMain from "./PageMain/PageMain";
 import IconButton from "./IconButton/IconButton";
+import Avatar from "./Avatar/Avatar";
 import LinkButton from "./LinkButton/LinkButton";
 import Logo from "./Logo/Logo";
 import NonFieldError from "./NonFieldError/NonFieldError";
-import Avatar from "./Avatar/Avatar";
 import Footer from "./Footer/Footer";
+import Sidebar from "./Sidebar/Sidebar";
 
 export {
   Page,
