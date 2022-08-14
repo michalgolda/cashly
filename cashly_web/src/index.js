9f948febc1dc2dd26718aa179669d08ff5d94058
michalgolda <michal.golda@hotmail.com>
2022-08-14 19:05:48 +0200

    feat: use 'StrictMode' component

--- cashly_web/src/index.js
+++ cashly_web/src/index.js
@@ -1,10 +1,15 @@
-import App from "./App";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<App />);
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
