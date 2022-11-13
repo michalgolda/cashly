fcc0aa7daf0a72b3b6e1c839ef1db1f99b9cd96f
michalgolda <michal.golda@hotmail.com>
2022-11-13 17:34:09 +0100

    feat: create custom scrollbar

--- cashly_web/src/styles/global.js
+++ cashly_web/src/styles/global.js
@@ -16,6 +16,27 @@ const GlobalStyle = createGlobalStyle`
         background-color: ${({ theme }) => theme.colors.gray200};
         font-weight: ${({ theme }) => theme.fontWeights.regular};
     }
+
+    ::-webkit-scrollbar {
+        width: 8px;
+    }
+
+    ::-webkit-scrollbar-track {
+        background-color: ${({ theme }) => theme.colors.gray400};
+    }
+
+    ::-webkit-scrollbar-thumb {
+        border-radius: 2px;
+        background: ${({ theme }) => theme.colors.primary400}; 
+    }
+
+    ::-webkit-scrollbar-thumb:hover {
+        background: ${({ theme }) => theme.colors.primary500}; 
+    }
+
+    ::-webkit-scrollbar-thumb:focus {
+        background: ${({ theme }) => theme.colors.primary300}; 
+    }
 `;
 
 export default GlobalStyle;
