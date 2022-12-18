8e0bcae4624b1e89af57fb7af6a6ebba29530c7a
michalgolda <michal.golda@hotmail.com>
2022-12-18 17:03:36 +0100

    feat: add show and hide animation to 'Modal' component

--- cashly_web/src/components/Modal/Modal.js
+++ cashly_web/src/components/Modal/Modal.js
@@ -1,13 +1,25 @@
+import { useRef } from 'react';
+import { CSSTransition } from 'react-transition-group';
+
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import PropTypes from 'prop-types';
 
+import './Modal.css';
 import * as S from './Modal.styled';
 
 function Modal({ children, show, onHide, ...props }) {
+  const nodeRef = useRef(null);
+
   return (
-    show && (
-      <S.Wrapper>
+    <CSSTransition
+      nodeRef={nodeRef}
+      in={show}
+      timeout={300}
+      classNames="modal"
+      unmountOnExit
+    >
+      <S.Wrapper ref={nodeRef}>
         <S.Modal {...props}>
           <S.Header>
             <S.HideButton onClick={onHide}>
@@ -17,7 +29,7 @@ function Modal({ children, show, onHide, ...props }) {
           <S.Body>{children}</S.Body>
         </S.Modal>
       </S.Wrapper>
-    )
+    </CSSTransition>
   );
 }
 
