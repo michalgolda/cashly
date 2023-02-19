06a1cb3350685ad6f694e21bc839383046290a50
Michał Gołda <michal.golda@hotmail.com>
2023-02-19 19:42:16 +0100

    feat: add 'styled-components'

--- cashly_web/src/pages/_app.js
+++ cashly_web/src/pages/_app.js
@@ -1,5 +1,14 @@
-import '@/styles/globals.css'
+import { ThemeProvider } from 'styled-components'
+
+import { GlobalStyle, theme } from '@/styles'
 
 export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />
+    return (
+        <>
+            <ThemeProvider theme={theme}>
+                <GlobalStyle />
+                <Component {...pageProps} />
+            </ThemeProvider>
+        </>
+    )
 }
