ddf78ee12dc7aa430158d042f9ca0e3aa339451f
Michał Gołda <michal.golda@hotmail.com>
2023-02-19 20:50:35 +0100

    feat: create 'List' component

---
+++ cashly_web/src/components/List.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+const List = styled.ul`
+    width: 100%;
+    display: grid;
+    row-gap: 16px;
+    column-gap: 1rem;
+    list-style: none;
+`
+
+export default List
