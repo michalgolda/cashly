2ea3b30379f533eb0b1659bf814bc909062231f1
Michał Gołda <michal.golda@hotmail.com>
2023-02-21 20:59:15 +0100

    feat: pass user data and logout handler to 'Sidebar' component

--- cashly_web/src/layouts/MainLayout/MainLayout.js
+++ cashly_web/src/layouts/MainLayout/MainLayout.js
@@ -1,12 +1,21 @@
+import { useSession } from 'next-auth/react'
+import { signOut } from 'next-auth/react'
+
 import Footer from '@/components/Footer/Footer'
 import Sidebar from '@/components/Sidebar/Sidebar'
 
 import { StyledContent, StyledMain } from './MainLayout.styled'
 
 export default function MainLayout({ children, ...props }) {
+    const { data: session } = useSession()
+    const onLogout = () => signOut({ callbackUrl: '/login' })
+
     return (
         <StyledMain {...props}>
-            <Sidebar />
+            <Sidebar
+                userEmail={session && session.user.email}
+                onLogout={onLogout}
+            />
             <StyledContent>
                 {children}
                 <Footer />
