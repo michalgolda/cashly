8c4b67cb66eb2ac076aeb86d18ed56684065aaae
Michał Gołda <michal.golda@hotmail.com>
2023-02-23 21:07:28 +0100

    fix: image elements must have an alt prop

--- cashly_web/src/views/Categories/components/List/EmptyInformer.js
+++ cashly_web/src/views/Categories/components/List/EmptyInformer.js
@@ -8,7 +8,14 @@ export default function EmptyInformer() {
     return (
         <Informer
             bottomElement={<AddButton />}
-            illustration={<Image src="/void.svg" width={128} height={133.94} />}
+            illustration={
+                <Image
+                    src="/void.svg"
+                    alt="empty list"
+                    width={128}
+                    height={133.94}
+                />
+            }
             text={'Aktualnie lista kategorii wydatków jest pusta'}
         />
     )
