28881e6cc7851f481b2241b3c7c59ee60d6b2f86
Michał Gołda <michal.golda@hotmail.com>
2023-02-23 21:04:39 +0100

    fix: image elements must have an alt prop

--- cashly_web/src/views/NotFound/NotFound.js
+++ cashly_web/src/views/NotFound/NotFound.js
@@ -10,7 +10,12 @@ export default function NotFound() {
         <StyledContainer>
             <Informer
                 illustration={
-                    <Image src="/notFound.svg" width={256} height={169.99} />
+                    <Image
+                        src="/notFound.svg"
+                        alt="not found"
+                        width={256}
+                        height={169.99}
+                    />
                 }
                 bottomElement={<LinkButton href="/">Powrót</LinkButton>}
                 text="Strona, której szukasz nie istnieje lub została przeniesiona"
