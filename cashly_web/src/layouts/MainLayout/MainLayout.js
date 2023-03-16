3bf741ea254a05c54987963ff204cd99f0ee0e69
Michał Gołda <michal.golda@hotmail.com>
2023-03-16 20:20:24 +0100

    feat: change user verification reminder

--- cashly_web/src/layouts/MainLayout/MainLayout.js
+++ cashly_web/src/layouts/MainLayout/MainLayout.js
@@ -1,37 +1,57 @@
 import { useSession } from 'next-auth/react'
 import { signOut } from 'next-auth/react'
+import Image from 'next/image'
+import { useMutation } from 'react-query'
+import { toast } from 'react-toastify'
 
-import EmailVerificationReminder from '@/components/EmailVerificationReminder/EmailVerificationReminder'
-import { useEmailVerificationReminder } from '@/components/EmailVerificationReminder/useEmailVerificationReminder'
+import { userService } from '@/api/services'
+import Button from '@/components/Button/Button'
 import Footer from '@/components/Footer/Footer'
+import Informer from '@/components/Informer/Informer'
+import PageMain from '@/components/PageMain/PageMain'
 import Sidebar from '@/components/Sidebar/Sidebar'
 
 import { StyledContent, StyledMain } from './MainLayout.styled'
 
+function UnVerified({ userEmail }) {
+    const notifySuccess = () =>
+        toast.success('Link weryfikacyjny został wysłany')
+
+    const mutation = useMutation(userService.sendEmailVerificationRequest, {
+        onSuccess: () => notifySuccess(),
+        onError: () => notifyUnhandledError(),
+    })
+
+    return (
+        <PageMain>
+            <Informer
+                text="Twoje konto jest niezweryfikowane. Aby móc w pełni korzystać z aplikacji musisz zweryfikować swoje konto poprzez kliknięcie w link, który zostanie przesłany po kliknięciu w ponniższy przycisk"
+                illustration={<Image src="email.svg" alt="email" fill />}
+                bottomElement={
+                    <Button
+                        onClick={() => mutation.mutate({ email: userEmail })}
+                    >
+                        Aktywuj
+                    </Button>
+                }
+            />
+        </PageMain>
+    )
+}
+
 export default function MainLayout({ children, ...props }) {
     const { data: session } = useSession()
-    const emailVerificationReminder = useEmailVerificationReminder()
 
     const email = session && session.user.email
     const emailIsVerified = session && session.user.email_is_verified
 
-    const onLogout = () =>
-        signOut({ callbackUrl: '/login' }).then(() =>
-            emailVerificationReminder.reset()
-        )
+    const onLogout = () => signOut({ callbackUrl: '/login' })
 
     return (
         <StyledMain {...props}>
             <Sidebar userEmail={email} onLogout={onLogout} />
             <StyledContent>
-                {!emailIsVerified && (
-                    <EmailVerificationReminder
-                        email={email}
-                        onHide={() => emailVerificationReminder.hide()}
-                        isHidden={emailVerificationReminder.isHidden}
-                    />
-                )}
-                {children}
+                {emailIsVerified ? children : <UnVerified userEmail={email} />}
                 <Footer />
             </StyledContent>
         </StyledMain>
