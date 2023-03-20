a502ae841af29fa13b99c4822d5f0e759f14369e
Michał Gołda <michal.golda@hotmail.com>
2023-03-20 20:14:09 +0100

    fix: add required labelText property value

--- cashly_web/src/views/Analytics/components/DatePeriodSelect/DatePeriodSelect.js
+++ cashly_web/src/views/Analytics/components/DatePeriodSelect/DatePeriodSelect.js
@@ -14,6 +14,7 @@ export default function DatePeriodSelect() {
     return (
         <Input
             as="select"
+            labelText="Zakres"
             defaultValue="lastTwentyMonths"
             onChange={handleChangePeriod}
             fullWidth
