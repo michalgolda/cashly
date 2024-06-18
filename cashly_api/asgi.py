32720788d0afbd56e51818e67fbb6ae86d638d6c
michalgolda <michal.golda@hotmail.com>
2024-06-18 20:53:33 +0200

    chore: change uvicorn app module pass way

--- cashly_api/asgi.py
+++ cashly_api/asgi.py
@@ -5,4 +5,4 @@ from app.settings import settings
 app = create_app()
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000, log_level="info")
+    uvicorn.run("asgi:app", host="0.0.0.0", port=8000, log_level="info")
