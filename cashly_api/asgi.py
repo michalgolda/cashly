1b90d00d0192cb7545ccb5c995a3b5565c982cb0
michalgolda <michal.golda@hotmail.com>
2024-06-18 18:45:35 +0200

    chore: add asgi.py file for running application on vercel

---
+++ cashly_api/asgi.py
@@ -0,0 +1,8 @@
+import uvicorn
+from app import create_app
+from app.settings import settings
+
+app = create_app()
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000, log_level="info")
